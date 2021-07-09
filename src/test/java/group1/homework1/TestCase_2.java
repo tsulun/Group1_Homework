3ac9beeea9ba76a8b9629e8fb690178d088aa8ea
yahyaayabakan <ayabakanyahya@gmail.com>
2021-07-09 17:56:44 +0100

    Before and after methods have been created

--- src/test/java/group1/homework1/TestCase_2.java
+++ src/test/java/group1/homework1/TestCase_2.java
@@ -1,4 +1,32 @@
 package group1.homework1;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 public class TestCase_2 {
+
+        WebDriver driver;
+
+        @BeforeMethod
+        public void setUp(){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        @AfterMethod
+        public void tearDown() throws InterruptedException {
+            Thread.sleep(2000);
+            driver.quit();
+        }
+
 }
