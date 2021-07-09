a756d78fee0a59e2113279d11b1a9b0df2d90ca3
yahyaayabakan <ayabakanyahya@gmail.com>
2021-07-09 16:39:18 +0100

    Created before and after methods

--- src/test/java/group1/homework1/TestCase_1.java
+++ src/test/java/group1/homework1/TestCase_1.java
@@ -1,4 +1,22 @@
 package group1.homework1;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class TestCase_1 {
-}
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+    }
+    @AfterMethod
+    public void tearDown(){
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+    }
