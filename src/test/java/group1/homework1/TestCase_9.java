80cfe2769201c72292bd63581d7950b38691f8ef
TeodoruMarin <teodoru652@gmail.com>
2021-07-10 21:01:05 +0100

    More work done tc9

--- src/test/java/group1/homework1/TestCase_9.java
+++ src/test/java/group1/homework1/TestCase_9.java
@@ -19,17 +19,32 @@ public class TestCase_9 {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        }
-        @AfterMethod
-        public void tearDown() throws InterruptedException {
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
 
         Thread.sleep(3000);
         driver.quit();
-
     }
 
+    @Test
+    public void test1() throws InterruptedException {
+        driver.get("https://practice-cybertekschool.herokuapp.com");
+        Thread.sleep(3000);
+
+        WebElement statusCodes = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/ul/li[46]/a"));
+        statusCodes.click();
 
 
+        WebElement twoHundred = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/ul/li[1]/a"));
+        twoHundred.click();
+
+
+
+
+
+    }
 
     }
 
